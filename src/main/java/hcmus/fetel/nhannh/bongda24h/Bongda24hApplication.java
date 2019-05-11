59f1293ea56d7d1067c7819415c11be909ace44e
Nguyen Hoang Nhan <ng.hnhan92@gmail.com>
2019-05-11 21:56:14 +0700

    Init project

---
+++ src/main/java/hcmus/fetel/nhannh/bongda24h/Bongda24hApplication.java
@@ -0,0 +1,13 @@
+package hcmus.fetel.nhannh.bongda24h;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bongda24hApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Bongda24hApplication.class, args);
+	}
+
+}
